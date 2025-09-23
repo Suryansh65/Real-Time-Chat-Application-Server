ce15b140baa0d550881c49bef164df99994d3457
Suryansh <suryansh652002@gmail.com>
2025-09-23 22:01:59 +0530

    Work in progress

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 // Create express app and HTTP server
 const app = express();
@@ -11,6 +12,9 @@ const server = http.createServer(app);
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 app.use("/api/status", (req, res) => res.send("server is live"));
+
+// Connect to mongodb
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
